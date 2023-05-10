8e7bf46880c9f02c51e8d71e544e91616fb8a539
5ecret0 <86318498+5ecret0@users.noreply.github.com>
2023-05-10 21:47:49 +0200

    Update nodejsDSL.groovy

--- DSL/nodejsDSL.groovy
+++ DSL/nodejsDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js DSL') {
     description('Aplicación Node JS DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/5ecret0/nodejsapp.git', 'main') { node ->
+            node / gitConfigName('secreto')
+            node / gitConfigEmail('cristianseco12@gmail.com')
         }
     }
     triggers {
